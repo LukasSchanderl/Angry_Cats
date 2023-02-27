c0e659126fb8484fb09966b0d76cf78d593675a6
Lukas Schanderl <lukas@veecoco.com>
2023-02-27 15:43:42 +0100

    Added some data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#CATS
+
+# name
+Faker::Creature::Cat.name
+#address
+Strausberger Pl. 79
+Saarbrücker Str. 82
+Karl-Liebknecht-Str. 15
+Auguststraße 58
+Am Zwirngraben 43
+Große Präsidentenstraße 28
+Am Kupfergraben 69
+Schendelgasse 50
+Kleine Hamburger Str. 56
+Gerichtstraße 67
+Segelfliegerdamm 22
+Leopoldstraße 52
+Lützowplatz 77
+Alexanderstraße 60
+Kleine Hamburger Str. 56
+Rosenthaler Str. 54
+Große Hamburger Str. 46
+Potsdamer Straße 58
+Rathausstraße 32
+Lindenstraße 44
+Genslerstraße 29
+Rosenthaler Str. 31
+
+#photo
+https://cataas.com/cat
+
+#fluffiness
+Faker::Number.within(range: 1..5)
+#color
+Faker::Color.color_name
+#price
+Faker::Number.decimal(l_digits: 2)
+
+#USERS
+
+#email
+Faker::Internet.email
+#BOOKINGS
+
+#start date
+Faker::Date.between(from: '2022-01-23', to: '2022-03-31')
+#end date
+Faker::Date.between(from: '2022-02-01', to: '2022-04-01')

c467edff38f9f4a121b29142014e37434d46f9eb
Lukas Schanderl <lukas@veecoco.com>
2023-03-01 16:49:14 +0100

    lukas seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,17 @@ i = 0
     fluffiness: Faker::Number.within(range: 1..5),
     color: Faker::Color.color_name,
     price: Faker::Number.decimal(l_digits: 2),
-    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    # description: Faker::Quotes::Shakespeare.hamlet_quote,
+    description: "In a small town, a cat came hiding in people's homes and people named it kitty. Kitty used to come to everyone's house and drink milk and sometimes eat food. People slowly started getting worried about this.
+    People thought of catching the kitty. For this, they laid a trap for the kitty.
+    At night, Kitty went out in search of her food but she fell into that trap. After being caught in the trap, she was kept in captivity for several days.
+    Kitty screamed fondly, 'meow meow meow meow'
+    Seeing Kitty upset, a girl named Lily thought of taking care of her and making her a pet. Lily brought Kitty to her home and put a ringing bell around her neck.
+    When Kitty moves her bell sounds, 'ting ting ting'.
+    Lily would give good food to the kitty every day and also give her milk. But Kitty enjoyed going to others' homes to eat stolen food.
+    Kitty started to go out at night and went to the others' houses. But now she gets nothing. People would hide the food after hearing the sound of her ringing the bell. Whatever the kitty tries, she does not get anything.
+    In such a situation, Kitty enlisted the help of her friend Rat. The rat bit the kitty's bell with his tooth. Rat helped kitty to get rid of a ringing bell.
+    Now the kitty was free again. People no longer know when the kitty comes and goes. Now Kitty is smarter than ever.",
   )
   cat.photo.attach(io: URI.open("https://cataas.com/cat"), filename: "test")
   cat.user = User.all[i]

a25e1a10f4a8aaf4d0336b71cddc636b0dd57a0a
marina belikova <memymilk@gmail.com>
2023-03-01 12:57:37 +0100

    renaming

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -33,6 +33,6 @@ class CatsController < ApplicationController
 
   private
   def cat_params
-    params.require(:cat).permit(:name, :pickup_address, :angriness_level, :fluffiness, :color, :price, :photo)
+    params.require(:cat).permit(:name, :address, :angriness_level, :fluffiness, :color, :price, :photo)
   end
 end

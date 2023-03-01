59efcff5ac3bbbe8a1fc70700d8761055969b1b2
marina belikova <memymilk@gmail.com>
2023-03-01 12:52:54 +0100

    grocoder setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_104326) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_115000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_104326) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
-    t.string "pickup_address"
+    t.string "address"
     t.integer "angriness_level"
     t.integer "fluffiness"
     t.string "color"

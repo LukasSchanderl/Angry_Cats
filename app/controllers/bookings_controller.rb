657e3aea6b7aeebb1fc28380e60677576919968f
Antje Stiller <antjestiller@gmx.de>
2023-03-02 17:43:50 +0100

    booking feature, show-booking, styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
 class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
+    @cat = Cat.find(params[:cat_id])
+
+    @sum_stars_angriness = @booking.cat.angriness_level
+    @sum_no_stars_angriness = 5 - @sum_stars_angriness
+
+    @sum_stars_fluffiness = @booking.cat.fluffiness
+    @sum_no_stars_fluffiness = 5 - @sum_stars_fluffiness
   end
 
   def new
@@ -15,8 +22,7 @@ class BookingsController < ApplicationController
     @booking.cat = @cat
 
     if @booking.save!
-      redirect_to root
-      raise
+      redirect_to cat_booking_path(@cat, @booking)
     else
       render :new
     end

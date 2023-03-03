d90c27e4691e4cf5314f5daad1ad36a8088eb559
marina belikova <memymilk@gmail.com>
2023-03-03 15:59:57 +0100

    angriness and fluffiness filters work

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -16,44 +16,62 @@ export default class extends Controller {
     "black",
     "white",
     "mixed",
-    "result"
   ];
 
   connect() {
-    console.log("Filter 2 from stimulus");
-    console.log(this.catsValue);
+    // console.log("Filter 2 from stimulus");
+    // console.log(this.catsValue);
   }
 
   apply(e) {
     e.preventDefault();
-    console.log(this.resultTarget)
 
-    // const colours = []
-    // const fluff = this.fluffTarget.value
-    // const angry = this.angryTarget.value
-    // const distance = this.distanceTarget.value
-    // const price = this.priceTarget.value
+    const colours = []
+    const fluff = this.fluffTarget.value
+    const angry = this.angryTarget.value
+    const distance = this.distanceTarget.value
+    const price = this.priceTarget.value
 
-    // // console.log(this.fluffTarget.value)
-    // // console.log(this.angryTarget.value)
-    // // console.log(this.priceTarget.value)
-    // // console.log(this.distanceTarget.value)
+    // console.log(this.fluffTarget.value)
+    // console.log(this.angryTarget.value)
+    // console.log(this.priceTarget.value)
+    // console.log(this.distanceTarget.value)
 
-    // const checkboxes = [this.redTarget, this.blackTarget, this.whiteTarget, this.mixedTarget];
+    const checkboxes = [this.redTarget, this.blackTarget, this.whiteTarget, this.mixedTarget];
 
-    // //console.log(this.redTarget.checked)
-    // checkboxes.forEach((checkbox) => {
-    //   if (checkbox.checked == true){
-    //     //  console.log(checkbox.checked)
-    //     colours.push(checkbox.value)
-    //   }else{
-    //     console.log("no")
-    //   }
-    // });
-    // // console.log(colours)
+    //console.log(this.redTarget.checked)
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked == true){
+        //  console.log(checkbox.checked)
+        colours.push(checkbox.value)
+      }else{
+        // console.log("no")
+      }
+    });
+    // console.log(colours)
 
-    // const inputs = { fluffiness: fluff, angriness_level: angry, distance: distance, price: price, colors: colours }
+    const inputs = { fluffiness: fluff, angriness_level: angry, distance: distance, price: price, colors: colours }
     // console.log(inputs)
+
+    // console.log(this.catsValue)
+    let kitties = [];
+
+    this.catsValue.forEach((cat) => {
+      // console.log(cat.fluffiness)
+      // console.log(this.fluffTarget.value)
+      if (cat.fluffiness >= this.fluffTarget.value){
+
+        if (cat.angriness_level >= this.angryTarget.value){
+          kitties.push(cat)
+          // console.log(kitties)
+
+        }
+
+
+      }
+    }
+    )
+
     // this.resultTarget.value = JSON.stringify(inputs)
   }
 }
